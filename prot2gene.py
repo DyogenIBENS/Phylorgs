a7ee23a35e56349454aeadad24e91425603f3293
Guillaume Louvel <7593801+Gullumluvl@users.noreply.github.com>
2016-11-21 17:57:31 +0100

    trying to fix Ciona savignyi case (without success). Add option --force-overwrite

--- prot2gene.py
+++ prot2gene.py
@@ -31,7 +31,7 @@ def convert_prot2species(modernID):
                 'ENSGALP': 'Gallus gallus',
                 'ENSMGAP': 'Meleagris gallopavo',
                 'ENSTGUP': 'Taeniopygia guttata',
-                'ENSFALP': 'Ficedulla albicollis',
+                'ENSFALP': 'Ficedula albicollis',
                 'ENSAPLP': 'Anas platyrhynchos',
                 'ENSACAP': 'Anolis carolinensis',
                 'ENSOANP': 'Ornithorhynchus anatinus',
@@ -127,12 +127,20 @@ def convert_prot2gene(protID):
 
 
 def rewrite_fastafile(fastafile, outputformat="{0}_genes.fa", cprot=2, cgene=0):
-    # avoid duplicate genes
-    found = {}
     genetree, ext = os.path.splitext(fastafile)
+    if ext == '.bz2': genetree, ext = os.path.splitext(genetree)
     genetreedir, genetreefile = os.path.split(genetree)
     #print >>sys.stderr, genetree, genetreedir, genetreefile
     outfile = outputformat.format(genetreefile)
+    if os.path.exists(outfile):
+        if force_overwrite:
+            print >>sys.stderr, "(Overwriting)"
+        else:
+            print >>sys.stderr, "Exists. Skipping."
+            return
+
+    # avoid duplicate genes
+    found = {}
     unknowns = 0
     with myopen(fastafile) as IN, myopen(outfile, 'w') as OUT:
         for line in IN:
@@ -142,6 +150,10 @@ def rewrite_fastafile(fastafile, outputformat="{0}_genes.fa", cprot=2, cgene=0):
                 if not geneID and protID.startswith('ENSCSAP'):
                     protID = protID.replace('ENSCSAP', 'ENSCSAVP')
                     geneID = convert_prot2gene(protID)
+                    print >>sys.stderr, "converting", geneID
+                    if geneID:
+                        # Fit names in tree
+                        geneID = geneID.replace('ENSCSAVG', 'ENSCSAG')
                 if not geneID:
                     unknowns += 1
                     geneID = "unknown_gene_%s" % unknowns
@@ -165,12 +177,15 @@ if __name__=='__main__':
                               "basename of the input file"))
     parser.add_argument("--shorten-species", action='store_true',
                         help="change 'Mus musculus' to 'mmusculus'?")
+    parser.add_argument("-f", "--force-overwrite", action='store_true',
+                        help="overwrite already existing files")
     parser.add_argument("--cprot", type=int, default=2, help="column for protein")
     parser.add_argument("--cgene", type=int, default=0, help="column for gene")
 
     args = parser.parse_args()
     gene_info = args.gene_info # global variable
     shorten_species = args.shorten_species # global variable
+    force_overwrite = args.force_overwrite
     #for protID in sys.argv[2:]:
     for fastafile in args.fastafiles:
         print >>sys.stderr, fastafile
