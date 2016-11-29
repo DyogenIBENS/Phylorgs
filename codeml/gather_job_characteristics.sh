547a611fe658435bcdeb57a25650683b3c670233
Guillaume Louvel <7593801+Gullumluvl@users.noreply.github.com>
2016-11-29 11:34:46 +0100

    Create a table of codeml runs characteristics (size of alignments (bytes, nb of seq, length of seq), time and memory consumed)

---
+++ codeml/gather_job_characteristics.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -e
+set -u
+
+echo -e 'name\tseq_bytes\tseq_nb\tseq_len\ttime\tmemory'
+
+# Number of leaves
+for ctlfile in *.ctl; do
+	base=`basename "$ctlfile" .ctl`
+	outfile="$base.condor.stdout"
+	logfile="$base.condor.log"
+	if [ -f "$outfile" -a -f "$logfile" ]; then
+		#treefile=`sed -rn 's/^\s*treefile\s*=\s*(\S+)[\s*]?.*$/\1/p' "$ctlfile"`
+		seqfile=`sed -rn 's/^\s*seqfile\s*=\s*(\S+)[\s*]?.*$/\1/p' "$ctlfile"`
+		if [ -z "$seqfile" ]; then
+			echo "Error: 'seqfile' not found in $ctlfile" >&2
+			exit 1
+		fi
+
+		# Get: bytesize of alignment, nb of sequences, length of sequences
+		echo -ne "$base\t"
+		echo -ne "`wc -c $seqfile | cut -d' ' -f1`\t"
+		echo -ne "`head -1 $seqfile | cut -f-2`\t"
+		
+		if grep -q 'Job terminated' "$logfile"; then
+			
+			# Time used:
+			grep '^Time used:' "$outfile" | \
+				sed 's/^Time used:\s\+//' | \
+				tr '\n' '\t'
+
+			# Memory used (MB)
+			grep '^\s\+Memory' "$logfile" | \
+				sed -r 's/^\s+Memory \(MB\)\s+:\s+([0-9]+)\s+[0-9]+\s+[0-9]+$/\1/'
+		else
+			echo -e '\t'
+		fi
+	else
+		echo "Codeml Job not started for $ctlfile" >&2
+	fi
+done
+
