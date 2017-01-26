1c4e075748b21eaf750f114008a28e3816de92ed
Guillaume Louvel <7593801+Gullumluvl@users.noreply.github.com>
2017-01-26 15:55:49 +0100

    corrected MAJOR flaw in 'prune2family': now it does not save subtrees already included in previous subtrees. Added --dry-run option

--- codeml/prune2family.py
+++ codeml/prune2family.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env python2.7
 
-#from Bio import Phylo
-#
-#def load_tree(filename):
-#    tree = Phylo.read(filename, "newick")
-#    return tree
-
+import re
 import sys
 import os.path
 import argparse
@@ -56,17 +51,23 @@ def add_species_nodes_back(tree, phyltree):
         # same ancestor as child is possible for duplication node
         # So check for length 1 or 2
         if len(ancestor_lineage) > 2:
+            # conserve original distance
+            n_new_branches = len(ancestor_lineage) - 1
+            dist_new_branches = float(node.dist) / n_new_branches
             # Add missing links
             for link in ancestor_lineage[1:-1]:
-                parent_node = parent_node.add_child(name=(link + genename))
+                parent_node = parent_node.add_child(name=(link + genename),
+                                                    dist=dist_new_branches)
             # Move the node on top of the created intermediate links
-            parent_node.add_child(child=node.detach())
+            parent_node.add_child(child=node.detach(),
+                                  dist=dist_new_branches)
 
 
-def search_by_ancestorspecies(tree, ancestor):
-    for node in tree.traverse():
-        if node.name.startswith(ancestor):
-            yield node
+def search_by_ancestorlist(tree, ancestorlist):
+    def stop_at_any_ancestor(node):
+        return any(node.name.startswith(anc) for anc in ancestorlist)
+    return tree.iter_leaves(is_leaf_fn=stop_at_any_ancestor)
+
 
 def save_subtrees_byspecieslist(tree, specieslist, outdir='.'):
     ancestor = PHYLTREE.lastCommonAncestor(specieslist)
@@ -81,7 +82,8 @@ def with_dup(leafnames):
     return (len(leafspecies) > len(set(leafspecies)))
 
 
-def save_subtrees(treefile, ancestors, outdir='.', only_dup=False):
+def save_subtrees(treefile, ancestors, outdir='.', only_dup=False,
+                  dry_run=False):
     try:
         tree = ete3.Tree(treefile, format=1)
     except ete3.parser.newick.NewickError as e:
@@ -90,15 +92,25 @@ def save_subtrees(treefile, ancestors, outdir='.', only_dup=False):
     add_species_nodes_back(tree, PHYLTREE)
     for ancestor in ancestors:
         ancestor = ancestor.capitalize()
-        for node in search_by_ancestorspecies(tree, ancestor):
+        ancestorlist = sorted(PHYLTREE.allDescendants[ancestor],
+                              key=lambda anc: -PHYLTREE.ages[anc])
+        ancestor_regex = re.compile('^(%s)(?=ENSGT)' % \
+                                    '|'.join(ancestorlist).replace(' ', '.'))
+        for node in search_by_ancestorlist(tree, ancestorlist):
             leafnames = node.get_leaf_names()
             #print node.name
             #print node.get_ascii()
             if len(leafnames) > 1:
+                # check that there is at least one duplication
                 if not only_dup or with_dup(leafnames):
-                    # check that there is at least one duplication
+                    outname = ancestor_regex.sub(node.name, ancestor)
                     outfile = os.path.join(outdir, node.name + '.nwk')
-                    node.write(format=1, format_root_node=True, outfile=outfile)
+                    if dry_run:
+                        print outfile
+                    else:
+                        node.write(format=1,
+                                   format_root_node=True,
+                                   outfile=outfile)
 
 
 if __name__ == '__main__':
@@ -109,6 +121,8 @@ if __name__ == '__main__':
     parser.add_argument("--only-dup", action="store_true",
                         help="do not extract trees that don't have at least "\
                              "one duplication")
+    parser.add_argument("-n", "--dry-run", action="store_true",
+                        help="only print out the output files it would produce")
     #parser.add_argument("-e", "--ensembl-version", type=int, default=85)
     #parser.add_argument("-p", "--phyltree", help="")
     args = parser.parse_args()
