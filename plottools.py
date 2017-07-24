259f8786884cb05e1d91223f290529700c105a71
Guillaume Louvel <7593801+Gullumluvl@users.noreply.github.com>
2017-07-24 19:09:12 +0200

    some useful plotting functions

---
+++ plottools.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Code snippets for matplotlib/pandas plotting."""
+
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+
+### Plotting style in matplotlib ###
+
+# Change all black to dark grey
+grey10 = '#1a1a1a'
+grey45 = '#737373'
+grey80 = '#CCCCCC'
+mpl.rcParams['text.color'] = grey10
+mpl.rcParams['axes.edgecolor'] = grey10
+mpl.rcParams['axes.labelcolor'] = grey10
+mpl.rcParams['axes.spines.top'] = False
+mpl.rcParams['axes.spines.right'] = False
+mpl.rcParams['xtick.color'] = grey10
+mpl.rcParams['ytick.color'] = grey10
+mpl.rcParams['grid.color'] = grey80
+mpl.rcParams['grid.linestyle'] = ':'
+mpl.rcParams['patch.edgecolor'] = grey10
+mpl.rcParams['boxplot.flierprops.markeredgecolor'] = grey10
+mpl.rcParams['boxplot.capprops.color'] = grey10
+mpl.rcParams['legend.facecolor'] = grey45
+mpl.rcParams['legend.framealpha'] = 0.2
+#mpl.rcParams['legend.edgecolor'] = grey10
+mpl.rcParams['savefig.facecolor'] = 'none'
+#mpl.rcParams['savefig.frameon'] = False  #background frame transparent
+#mpl.rcParams['savefig.transparent'] = True # all background transparent
+                                            # (including ggplot2 style)
+#mpl.style.use('ggplot')
+#pd.set_option('display.max_colwidth', 85)
+
+
+### Helping functions ###
+
+def plotby(df, by, kind='line', ncols=3, sharex=True, sharey=True, **kwds):
+    df_grouped = df.groupby(by)
+    ngroups = len(df_grouped)
+    nrows = ngroups // ncols + 1*(ngroups % ncols > 0)
+    fig, axes = plt.subplots(nrows, ncols, sharex, sharey)
+    #print(kde_axes, type(kde_axes))
+    for i, (title, data) in enumerate(df_grouped):
+        ax = axes[i // ncols, i % ncols]
+        subkwds = dict((key,v[i]) if isinstance(v, list)
+                       else (key, v[title]) if isinstance(v, dict)
+                       else(key,v)
+                       for key,v in kwds.items())
+        data.plot(kind=kind, ax=ax, title=title, **subkwds)
+
+    # Hide unnecessary axes
+    for j in range(i+1, nrows*ncols):
+        ax = axes[j // ncols, j % ncols]
+        ax.axis('off')
+
+    return axes
+
+# TODO:
+def ordered_boxplot(df, x, y, order=None, **kwds):
+    pass
