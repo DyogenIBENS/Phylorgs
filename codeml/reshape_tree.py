712f97ce6639c6367fcda1dfd31d6de24d3b1002
Guillaume Louvel <7593801+Gullumluvl@users.noreply.github.com>
2017-09-29 15:40:27 +0200

    removes internal node labels but keep the codeml marks.

--- codeml/reshape_tree.py
+++ codeml/reshape_tree.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2.7
+#!/usr/bin/env python3
 
 """USAGE:
     ./reshape_tree.py [-h] <input_tree> <output_tree>
@@ -10,6 +10,7 @@ DESCRIPTION:
     produces reshaped newick tree compatible with codeml:
     Reshaping means removing the inner node labels, and removing nodes that
     have only one child (codeml fails on such a tree).
+    Keep foreground branch labels such as '#1', '#2' or '$1'.
     Requires the python module 'ete3'.
 NOTE:
     It is not an equivalent of reshape_tree.sh, since the latter only removes
@@ -17,18 +18,21 @@ NOTE:
 """
 
 import sys
+import re
 import ete3
 
 
 if __name__=='__main__':
     if len(sys.argv) != 3:
-        print __doc__
+        print(__doc__)
         if len(sys.argv) == 2 and sys.argv[1] in ('-h', '--help'):
             sys.exit(0)
         sys.exit(1)
 
     input_tree = sys.argv[1]
     output_tree = sys.argv[2]
+
+    re_mark = re.compile(r'[#$][0-9]')
 
     tree = ete3.Tree(input_tree, format=1)
     for node in tree.traverse():
@@ -38,5 +42,15 @@ if __name__=='__main__':
     for node in newroot.traverse():
         if len(node.children) == 1:
             node.delete(prevent_nondicotomic=False, preserve_branch_length=True)
+        elif not node.is_leaf():
+            m = re_mark.search(node.name)
+            if m:
+                node.name = m.group()
+            else:
+                node.name = ''
+            
+
     # The format compatible with codeml is format=5
-    newroot.write(outfile=output_tree, format=5)
+    #newroot.write(outfile=output_tree, format=5)
+    newroot.write(outfile=output_tree, format=1)
+
