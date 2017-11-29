10b2004398a89f1004303e028643767fb3d4cd70
Guillaume Louvel <7593801+Gullumluvl@users.noreply.github.com>
2017-11-29 16:23:43 +0100

    Catch exceptions and close connections properly

--- query_align_cds.pl
+++ query_align_cds.pl
@@ -8,9 +8,8 @@ use Bio::EnsEMBL::Registry;
 
 use IO::Compress::Bzip2 qw(bzip2 $Bzip2Error) ;
 
-
 # Set autoflush;
-#$| = 1;
+$| = 1;
 
 
 my $db_version = $ARGV[0] || 86 ;
@@ -25,6 +24,30 @@ $reg->load_registry_from_db(
     -user=>'anonymous', 
     -db_version=>$db_version,
 );
+
+sub clean_exit {
+	my ($sig) = @_;
+	print STDERR "Caught a SIG$sig. Closing connections and exiting.";
+	$reg->disconnect_all(); # NOT SURE HERE. CHECK.
+	exit(1);
+}
+
+sub clean_die {
+	my ($msg) = @_;
+	print STDERR "Die for reason: $msg.\nClosing connections and exiting.";
+	$reg->disconnect_all(); # NOT SURE HERE. CHECK.
+	exit(1);
+}
+
+#$SIG{'INT'}   = \&clean_exit; # Those signals are caught by sigtrap normal-signals already
+#$SIG{'TERM'}  = \&clean_exit;
+$SIG{__DIE__} = \&clean_die;
+use sigtrap qw(handler clean_exit normal-signals error-signals);
+#use sigtrap qw(handler clean_exit error-signals);
+
+# Arg [1]     : name of the species to add the adaptor to in the registry.
+# Arg [2]     : name of the group   to add the adaptor to in the registry.
+# Arg [3]     : name of the type    to add the adaptor to in the registry.
 my $tree_adaptor = $reg->get_adaptor('Multi', 'compara', 'GeneTree'); 
 
 
@@ -40,7 +63,7 @@ while (my $GeneTree = <STDIN>) {
 	$count++ ;
 	if ( (! -f $outfile) && (! -f "$outfile.bz2") ) {
 		print STDERR "$count : Downloading $GeneTree.\n"; # Compressing '$prev_outfile'\n" ;		
-
+		# fetch_all?
 		my $tree = $tree_adaptor->fetch_by_stable_id($GeneTree);
 		$tree->preload;
 
@@ -59,13 +82,14 @@ while (my $GeneTree = <STDIN>) {
 		#		or die "bzip2 $prev_outfile failed $?\nReason: $!\n";
 		#}
 
-		my $alignIO = Bio::AlignIO->newFh(-file => ">$outfile", -format => "fasta");
+		my $alignIO = Bio::AlignIO->new(-file => ">$outfile", -format => "fasta");
 		#my $z = new IO::Compress::Bzip2 $alignIO
 		#	or die "IO::Compress::Bzip2 failed: $Bzip2Error\n";;
 		#print $z $cds_align;
 		#my $alignIO = Bio::AlignIO->newFh(-file => $z, -format => "fasta");
-		print $alignIO $cds_align;
+		$alignIO->write_aln($cds_align);
 		bzip2 "$outfile" => "$outfile.bz2" or die "bzip2 failed: $Bzip2Error\n";
+
 		system("rm $outfile") == 0 or die "rm failed ($?): $!\n";
 
 		#$prev_outfile = $outfile ;
@@ -81,3 +105,4 @@ while (my $GeneTree = <STDIN>) {
 #}
 
 #close(INFILE)
+$reg->disconnect_all(); # NOT SURE HERE. CHECK.
