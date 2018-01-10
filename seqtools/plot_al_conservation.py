59c0caba7a46d0984a40c18e5699b3f80729cf0f
Guillaume Louvel <7593801+Gullumluvl@users.noreply.github.com>
2018-01-10 18:28:32 +0100

    new utility for visualizing alignment caracteristics

---
+++ seqtools/plot_al_conservation.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+from sys import stderr
+
+import os.path
+import numpy as np
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+from Bio import AlignIO
+import argparse
+
+
+def filename2format(filename):
+    _, ext = os.path.splitext(filename)
+    return ext2fmt[ext]
+
+
+def al2list(align_col):
+    """Convert an alignment object to a list of strings"""
+    return [str(record.seq) for record in align_col]
+
+
+def proportions(values):
+    """ Compute proportion of each possible value.
+    return dictionary with proportion of each value.
+    values: list of elements."""
+    value_set = set(values)
+    L = len(values)
+    return {val: float(values.count()) / L}
+
+
+def np_proportions(values):
+    # Convert values to integers
+    vunique, vcounts = np.unique(values, return_counts=True)
+    return vunique, vcounts.astype(float) / len(values)
+
+
+def entropy(values, na=None):
+    value_prop = proportions(values)
+    return -sum(p*np.log2(p) for val, p in value_prop.items() if val != na)
+
+
+def np_entropy_subfunc(value_unique, value_prop, na=None):
+    if na is None:
+        na_pos = np.ones(len(value_unique)).astype(bool)
+    else:
+        na_pos = value_unique == na
+
+    return - (value_prop * np.log2(value_prop))[~na_pos].sum()
+
+
+def np_entropy(values, na=None):
+    value_unique, value_prop = np_proportions(values)
+    return np_entropy_subfunc(value_unique, value_prop, na)
+
+
+def al_stats(align, nucl=False):
+    """Compute gap proportion and entropy value for each position of the alignment"""
+    al_len = align.get_alignment_length()
+    if not al_len % 3:
+        print("Not a codon alignment!", file=stderr)
+
+    gap = '-'     if nucl else '---'
+    npos = al_len if nucl else al_len // 3
+    step = 1      if nucl else 3
+
+    gap_prop = np.zeros(npos)
+    al_entropy = np.ones(npos) * np.NaN
+
+    for i in range(npos):
+        value_unique, value_prop = np_proportions(values)
+        gap_prop[i]   = value_prop[np.argmax(value_unique == gap)] or 0
+        al_entropy[i] = np_entropy_subfunc(value_unique, value_prop, gap)
+
+    return gap_prop, al_entropy
+
+
+def plot_al_stats(gap_prop, al_entropy, outfile=None):
+    """"""
+    if outfile is None:
+        plt.switch_backend('QtAgg')
+
+    fig, axes = plt.subplots(3, share_x=True)
+
+    axes[0].plot(gap_prop)
+    axes[1].plot(al_entropy)
+    axes[2].plot((1-gap_prop) * (1 - al_entropy))
+
+    if outfile is None:
+        plt.show()
+    else:
+        fig.savefig(outfile)
+
+
+def main(infile, outfile=None, format=None, nucl=False):
+    align = AlignIO.read(infile, format=(format or filename2format(infile.name)))
+    gap_prop, al_entropy = al_stats(align, nucl)
+    plot_al_stats(gap_prop, al_entropy, outfile)
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('infile', nargs='?', default=sys.stdin,
+                        type=argparse.FileType('r'))
+    parser.add_argument('-o', '--outfile')
+    parser.add_argument('-f', '--format', help='Force format usage.' \
+                        ' Can be any format accepted by Bio.alignIO')
+    parser.add_argument('-n', '--nucl', action='store_true',
+                        help='Process per nucleotide position (instead of codon)')
+    
+    
+    args = parser.parse_args()
+    main(**vars(args))
