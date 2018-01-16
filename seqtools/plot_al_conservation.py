e940e9b76a50692751f0fc24539e27fb1056e28d
Guillaume Louvel <7593801+Gullumluvl@users.noreply.github.com>
2018-01-16 19:23:26 +0100

    add more comparison metrics (pearson, sp score)

--- seqtools/plot_al_conservation.py
+++ seqtools/plot_al_conservation.py
@@ -6,11 +6,11 @@ from sys import stderr, stdin
 
 import os.path
 
-from itertools import product
+from itertools import product, combinations
 
 import numpy as np
 import matplotlib as mpl
-mpl.use('Agg')
+mpl.use('Agg', warn=False)
 import matplotlib.pyplot as plt
 from Bio import AlignIO, Align, Alphabet
 import argparse
@@ -25,12 +25,33 @@ CODONS = [''.join(codon) for codon in product(*['ACGT']*3)]
 NACODON = '---'
 CODON2INT = {codon:i for i,codon in enumerate([NACODON] + CODONS)}
 NUCL2INT  = {'-': 0, 'A': 1, 'C': 2, 'G': 3, 'T': 4}
+STOPS = ['TAA', 'TAG', 'TGA']
 
 # Does not work...
 # Reading the alignment and issuing align[0][0] still yields a single nucleotide.
 codonalphabet = Alphabet.Alphabet()
 codonalphabet.letters = [NACODON] + CODONS
 codonalphabet.size = 3
+
+def make_unif_codon_dist():
+    all_values = [NACODON] + CODONS
+    uniform_codon_dist = 1 - np.diagflat(np.ones(len(all_values)))
+    for i,j in combinations(range(len(all_values)), 2):
+        # sum of nucleotide differences
+        cod0, cod1 = all_values[i], all_values[j]
+        pair_dist = (np.array(list(cod0)) != np.array(list(cod1))).sum()
+        uniform_codon_dist[i, j] = uniform_codon_dist[j, i] = pair_dist
+    
+    for stop in STOPS:
+        uniform_codon_dist[:, all_values.index(stop)] = np.NaN
+        uniform_codon_dist[all_values.index(stop), :] = np.NaN
+
+    return uniform_codon_dist
+
+UNIF_CODON_DIST = make_unif_codon_dist()
+
+#def read_codon_matrix(filename):
+#    # ~/install/jprime-extra/arvecodon.txt
 
 
 def al2array(align, nucl=False):
@@ -56,11 +77,16 @@ def category2int(array, converter_dict):
     """Convert an array of categorical values using a dictionary"""
     return np.vectorize(converter_dict.__getitem__)(array)
 
+
+def count_matrix(vint, minlength=65):
+    """column-wise matrix of counts of integers"""
+    assert vint.dtype == int
+    return np.stack([np.bincount(vint[:,i], minlength=minlength) \
+                        for i in range(vint.shape[1])], axis=-1)
+
 def freq_matrix(vint, minlength=65):
     """Convert matrix of integers to frequencies (per column)"""
-    counts = np.stack([np.bincount(vint[:,i], minlength=minlength) \
-                        for i in range(vint.shape[1])], axis=-1)
-    return counts.astype(float) / np.alen(vint)
+    return count_matrix(vint, minlength).astype(float) / np.alen(vint)
 
 def filename2format(filename):
     _, ext = os.path.splitext(filename)
@@ -91,19 +117,47 @@ def entropy(values, na=None):
     value_prop = proportions(values)
     return -sum(p*np.log2(p) for val, p in value_prop.items() if val != na)
 
-
+# Not used in `main` anymore
 def np_entropy_subfunc(value_unique, value_prop, na=None):
     if na is None:
-        na_pos = np.ones(len(value_unique)).astype(bool)
+        na_pos = np.full(len(value_unique), True)
     else:
         na_pos = value_unique == na
 
     return - (value_prop * np.log2(value_prop))[~na_pos].sum()
 
-
+# Not used in `main`
 def np_entropy(values, na=None):
     value_unique, value_prop = np_proportions(values)
     return np_entropy_subfunc(value_unique, value_prop, na)
+
+
+def pairs_score(vint, indexpairs, dist_mat=UNIF_CODON_DIST):
+    nrows, ncols = vint.shape
+    pairs = [np.stack((vint[i,:], vint[j,:])) for i, j in indexpairs]
+    pair_scores = np.array([[dist_mat[tuple(p[:,i])] for i in range(ncols)] for p in pairs])
+    print(pair_scores)
+    return pair_scores.sum(axis=0)
+
+def sp_score(vint, dist_mat=UNIF_CODON_DIST):
+    """Compute the sum of all pairs score, column-wise"""
+    return pairs_scores(vint, combinations(range(nrows), 2), dist_mat)
+
+def split_score(vint, split, dist_mat=UNIF_CODON_DIST):
+    assert len(split) == 2
+    return pairs_score(vint, product(*split), dist_mat)
+
+def cov(X,Y, axis=0):
+    mx = X.mean(axis=axis)
+    my = Y.mean(axis=axis)
+    return ((X - mx) * (Y - my)).mean(axis=axis)
+
+
+def pearson_coeff(X, Y, axis=0):
+    """Column-wise pearson coefficient"""
+    coeff = cov(X, Y, axis)
+    coeff /= (X.std(axis=axis) * Y.std(axis=axis))
+    return coeff
 
 
 def comp_parts(alint, compare_parts=None):
@@ -118,10 +172,14 @@ def comp_parts(alint, compare_parts=None):
     alparts = [np.stack([alint[i,:] for i in part]) for part in parts]
 
     freq_mat1, freq_mat2 = [freq_matrix(alpart) for alpart in alparts]
+    count_mat1, count_mat2 = [count_matrix(alpart) for alpart in alparts]
 
     manh_dist = np.abs(freq_mat1 - freq_mat2).sum(axis=0)
+    pearson_c = pearson_coeff(count_mat1, count_mat2, axis=0)
+    split_sc = split_score(alint, parts)
+    print(split_sc.shape)
 
-    return manh_dist
+    return np.stack((manh_dist, split_sc,))
 
 
 def al_stats(align, nucl=False):
@@ -135,9 +193,10 @@ def al_stats(align, nucl=False):
     step = 1      if nucl else 3
 
     alint = al2int(align, nucl)
+    alint = alint[:, alint.sum(axis=0) > 0]
 
     #gap_prop = np.zeros(npos)
-    #al_entropy = np.ones(npos) * np.NaN
+    #al_entropy = np.full(npos, np.NaN)
 
     #for i in range(npos):
     #    values = al2list(align[:, (step*i):(step*(i+1))])
@@ -150,6 +209,7 @@ def al_stats(align, nucl=False):
     gap_prop = al_freqs[0,:]
     # Convert zeros to ones so that x * log(x) = 0
     al_freqs = al_freqs[1:,:]
+    # scipy.stats.entropy
     al_freqs[al_freqs == 0] = 1
     al_entropy = - (al_freqs * np.log2(al_freqs)).sum(axis=0)
 
@@ -177,7 +237,7 @@ def plot_al_stats(gap_prop, al_entropy, alint, dist_array=None, seqlabels=None,
     masked_al = np.ma.array(alint, mask=(alint==0))
 
     nplots = 4 if dist_array is None else 5
-    fig, axes = plt.subplots(nplots, sharex=True)
+    fig, axes = plt.subplots(nplots, sharex=True, figsize=(15,10))
 
     x = np.arange(len(gap_prop))
     axes[0].step(x, gap_prop, where='post')
@@ -197,7 +257,13 @@ def plot_al_stats(gap_prop, al_entropy, alint, dist_array=None, seqlabels=None,
     axes[-1].set_xlabel("Residue position")
 
     if dist_array is not None:
-        axes[4].step(x, dist_array, where='post')
+        ax5 = axes[4].twinx()
+        l4 = axes[4].step(x, dist_array[0,:], color='#1f77b4', where='post',
+                     alpha=0.7, label='manhattan dist')
+        l5 = ax5.step(x, dist_array[1,:], color='#ff7f0e', where='post',
+                     alpha=0.7, label='pearson corr')
+        axes[4].legend(fontsize='xx-small')
+        ax5.legend(fontsize='xx-small')
     
     if outfile is None:
         plt.show()
@@ -218,7 +284,7 @@ def main(infile, outfile=None, format=None, nucl=False, records=None,
     seqlabels = [record.name for record in align]
     gap_prop, al_entropy, alint = al_stats(align, nucl)
     dist_array = comp_parts(alint, compare_parts)
-    print(dist_array)
+    #print(dist_array)
     plot_al_stats(gap_prop, al_entropy, alint, dist_array, seqlabels, outfile)
 
 
