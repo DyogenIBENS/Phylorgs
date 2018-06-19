839e1d5334acc6486e5120ec638707499a391c15
Guillaume Louvel <7593801+Gullumluvl@users.noreply.github.com>
2018-06-19 19:31:21 +0200

    Libsdyogen tree forest to treebest newick conversion

---
+++ treetools/genomicus2treebest.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+
+"""Convert LibsDyogen treeforest to TreeBest format (newick with special tags).
+
+USAGE:
+    
+    ./genomicus2treebest.py forestfile
+"""
+
+from __future__ import print_function
+
+
+from sys import argv, exit, stderr, stdout
+import LibsDyogen.myProteinTree as ProteinTree
+
+if len(argv) != 2:
+    print(__doc__, file=stderr)
+    exit(1)
+elif argv[1] in ('-h', '--help'):
+    print(__doc__, file=stderr)
+    exit()
+
+forestfile=argv[1]
+
+for tree in ProteinTree.loadTree(forestfile):
+    #for node, children in tree.data.items():
+    #    print(node, children)
+    tree.printNewick(stdout, withDist=True, withTags=True)
