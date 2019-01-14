02b5b04c04ee23adbbda732c53286636f8d998e0
Guillaume Louvel <7593801+Gullumluvl@users.noreply.github.com>
2019-01-14 19:25:50 +0100

    Read a single multi-newick file instead of multiple files

--- dendron/add_branch.py
+++ dendron/add_branch.py
@@ -9,13 +9,16 @@ logger = logging.getLogger(__name__)
 #logging.basicConfig(format="%(levelname)s:%(message)s")
 
 
-def main(infile, format=1, *args):
+def main(infile, subtreesfile=None, format=1, quoted_node_names=False):
     tree = ete3.Tree(infile, format=format)
-    if not args:
-        args = stdin
 
-    for arg in args:
-        newsubtree = ete3.Tree(arg, format=format)
+    stream = stdin if subtreesfile is None else open(subtreesfile)
+    subtrees = [s + ';' for s in stream.read().split(';')]
+    if subtreesfile is None:
+        stream.close()
+
+    for subtreetxt in subtrees:
+        newsubtree = ete3.Tree(subtreetxt, format=format, quoted_node_names=quoted_node_names)
         # First leaf of newsubtree should be an existing node in the main tree.
         ref_leaf = newsubtree.get_leaves()[0]
         assert ref_leaf == newsubtree.children[0], "incorrect subtree specification"
@@ -47,11 +50,13 @@ def main(infile, format=1, *args):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('infile')
-    parser.add_argument('subtrees', nargs='?', default=[],
+    parser.add_argument('subtreesfile', nargs='?',
                         help='subtrees to insert. The root should be the new ' \
                              'branching node, and the top leaf should be an ' \
                              'existing node.')
     parser.add_argument('-f', '--format', type=int, default=1)
+    parser.add_argument('-q', '--quoted-node-names', '--quoted', action='store_true')
     
     args = parser.parse_args()
-    main(args.infile, args.format, *args.subtrees)
+    print(args)
+    main(args.infile, args.subtreesfile, args.format, args.quoted_node_names)
