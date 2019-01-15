fba34b7e32eac2193b5c6edd7b9e5f66e8e60315
Guillaume Louvel <7593801+Gullumluvl@users.noreply.github.com>
2019-01-15 19:45:01 +0100

    1. compartiment into subfunctions, to make importable utilities. 2. Do not use preserve_branch_length option. Instead, transfer node dist to children, **not to parent**.

--- dendron/make_polytomies.py
+++ dendron/make_polytomies.py
@@ -5,9 +5,13 @@
 from sys import stdin
 import argparse as ap
 import ete3
+from dendron.climber import iter_distleaves
+import logging
+logger = logging.getLogger(__name__)
+logging.basicConfig()
 
 
-def main(conversionfile, treefile=None):
+def load_conversion(conversionfile):
     with open(conversionfile) as stream:
         conversion = {}
         for line in stream:
@@ -15,20 +19,56 @@ def main(conversionfile, treefile=None):
                 field1, field2 = line.rstrip().split('\t')
                 if field1 and field2:
                     conversion[field1] = field2
+    return conversion
 
-    if treefile is None:
-        treefile = stdin.read()
-    tree = ete3.Tree(treefile, format=1)
-    
+
+def get_data(tree, nodedist):
+    return [(ch, ch.dist) for ch in nodedist[0].children]
+
+
+def make_polytomies(tree, conversion):
     for node in tree.traverse('postorder'):
         newname = conversion.get(node.name, node.name)
         if '+' in newname:
             # This clade is polyphyletic in the reference tree.
             # Transform the parent into a multifurcation.
-            node.delete(prevent_nondicotomic=False, preserve_branch_length=True)
+            
+            # Memorize the original distances to leaves, to check if it preserves lengths
+            parent = node.up if node.up else node
+            distleaves0 = sorted(iter_distleaves(parent, parent, get_data),
+                                 key=lambda dl: dl[0].name)
+
+            # Propagate the deleted branch length to its children.
+            for child in node.children:
+                child.dist += node.dist
+            node.delete(prevent_nondicotomic=False, preserve_branch_length=False)
             #TODO: check that newname.split('+') matches the node.children.
+            logger.info("Delete node %r:%g (%s)", node.name, node.dist, newname)
+
+            distleaves = sorted(iter_distleaves(parent, parent, get_data),
+                                key=lambda dl: dl[0].name)
+
+            assert all((l0 == l) for (l0, dl0), (l, dl) in zip(distleaves0, distleaves)), \
+                ('\n'.join('%15s\t%15s' % (l0.name, l.name)
+                           for (l0,_), (l,_) in zip(distleaves0, distleaves)))
+            assert all( ((dl0-dl) < 1e-5) for (l0, dl0), (l, dl) in zip(distleaves0, distleaves)),\
+                ('Leaf distances were not preserved after deletion!!!\n'
+                 'At node %r:%g\n' % (node.name, node.dist)
+                 + '\n'.join('%15s:%g\t%15s:%g' % (l0.name, dl0, l.name, dl)
+                             for (l0,dl0), (l,dl) in zip(distleaves0, distleaves)))
+
+
+def main(conversionfile, treefile=None):
+    conversion = load_conversion(conversionfile)
+
+    if treefile is None:
+        treefile = stdin.read()
+    tree = ete3.Tree(treefile, format=1)
+    
+    make_polytomies(tree, conversion)
 
     print(tree.write(format=1, format_root_node=True))
+
 
 if __name__ == '__main__':
     parser = ap.ArgumentParser(description=__doc__)
--- dendron/rename_nodes.py
+++ dendron/rename_nodes.py
@@ -7,7 +7,7 @@ import argparse as ap
 import ete3
 
 
-def main(conversionfile, treefile=None):
+def load_conversion(conversionfile):
     with open(conversionfile) as stream:
         conversion = {}
         for line in stream:
@@ -15,18 +15,28 @@ def main(conversionfile, treefile=None):
                 field1, field2 = line.rstrip().split('\t')
                 if field1 and field2:
                     conversion[field1] = field2
+    return conversion
 
-    if treefile is None:
-        treefile = stdin.read()
-    tree = ete3.Tree(treefile, format=1)
-    
+
+def rename(tree, conversion):
     for node in tree.traverse():
         try:
             node.name = conversion[node.name]
         except KeyError:
             pass
 
+
+def main(conversionfile, treefile=None):
+    conversion = load_conversion(conversionfile)
+
+    if treefile is None:
+        treefile = stdin.read()
+    tree = ete3.Tree(treefile, format=1)
+    
+    rename(tree, conversion)
+
     print(tree.write(format=1, format_root_node=True))
+
 
 if __name__ == '__main__':
     parser = ap.ArgumentParser(description=__doc__)
