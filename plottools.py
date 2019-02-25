45a2bde296b3f7dfdbf13eed25a187b9731f6b09
Guillaume Louvel <7593801+Gullumluvl@users.noreply.github.com>
2019-02-25 23:48:57 +0100

    paste code for kde_ridgeplot (joyplot)

--- plottools.py
+++ plottools.py
@@ -9,6 +9,8 @@ import matplotlib.pyplot as plt
 from scipy.stats import gaussian_kde
 import pandas as pd
 
+import pandas as pd
+import seaborn as sns
 
 ### Plotting style in matplotlib ###
 
@@ -144,3 +146,51 @@ def dendrogram():
     ax.spines['top'].set_visible(False)
     ax.spines['bottom'].set_visible(False)
     ax.spines['right'].set_visible(False)
+
+
+# Or joypy.joyplot
+def kde_ridgeplot(x, by, data=None):
+    """
+    Overlapping densities ('ridge plot')
+    ====================================
+    
+    From: https://github.com/mwaskom/seaborn/blob/master/examples/kde_ridgeplot.py
+    """
+    sns.set(style="white", rc={"axes.facecolor": (0, 0, 0, 0)})
+
+    ## Create the data
+    #rs = np.random.RandomState(1979)
+    #x = rs.randn(500)
+    #g = np.tile(list("ABCDEFGHIJ"), 50)
+    #df = pd.DataFrame(dict(x=x, g=g))
+    #m = df.g.map(ord)
+    #df["x"] += m
+
+    df = data
+
+    # Initialize the FacetGrid object
+    pal = sns.cubehelix_palette(10, rot=-.25, light=.7)
+    g = sns.FacetGrid(df, row=by, hue=by, aspect=15, height=.5, palette=pal)
+
+    # Draw the densities in a few steps
+    g.map(sns.kdeplot, x, clip_on=False, shade=True, alpha=1, lw=1.5, bw=.2)
+    g.map(sns.kdeplot, x, clip_on=False, color="w", lw=2, bw=.2)
+    g.map(plt.axhline, y=0, lw=2, clip_on=False)
+
+
+    # Define and use a simple function to label the plot in axes coordinates
+    def label(x, color, label):
+        ax = plt.gca()
+        ax.text(0, .2, label, fontweight="bold", color=color,
+                ha="left", va="center", transform=ax.transAxes)
+
+
+    g.map(label, x)
+
+    # Set the subplots to overlap
+    g.fig.subplots_adjust(hspace=-.25)
+
+    # Remove axes details that don't play well with overlap
+    g.set_titles("")
+    g.set(yticks=[])
+    g.despine(bottom=True, left=True)
