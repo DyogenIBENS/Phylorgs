c7b8644b1ff29e3c94cfe3569b0bb4c1fdb98197
Guillaume Louvel <7593801+Gullumluvl@users.noreply.github.com>
2019-06-06 19:44:43 +0200

    Quick hack to append the species name to the sequence name (TreeBest format) in the **tree** as well.

--- seqtools/specify.py
+++ seqtools/specify.py
@@ -13,27 +13,62 @@ from genomicustools.identify import ultimate_seq2sp
 ENSEMBL_VERSION = 87
 
 
-def iter_specify(inputfile, format="fasta", dots=True, ensembl_version=ENSEMBL_VERSION):
+def iter_specify(inputfile, labelfmt='{gene}_{sp}', format="fasta",
+                 dots=True, ensembl_version=ENSEMBL_VERSION):
+#def iter_specify(iterator, name_attr='.id', **)
+    #yield element, (newname)
+
+    seen_labels = {}
     for record in SeqIO.parse(inputfile, format):
         sp = ultimate_seq2sp(record.id, ensembl_version)
         if dots:
             sp = sp.replace(' ', '.')
-        record.id += "_" + sp
+        record.id = labelfmt.format(gene=record.id, sp=sp)
+
+        if record.id in seen_labels:
+            record.id += '.%d' % seen_labels[record.id]
+            seen_labels[record.id] += 1
+        else:
+            seen_labels[record.id] = 1
+
         record.description = ''
         #print(record.id)
         yield record
 
 
-def specify(inputfile, outfile, format="fasta", dots=True,
+def specify(inputfile, outfile, labelfmt='{gene}_{sp}', format="fasta", dots=True,
             ensembl_version=ENSEMBL_VERSION):
-    SeqIO.write(iter_specify(inputfile, format, dots, ensembl_version),
-                outfile, format)
 
+    if format == 'nwk':
+        import ete3
+        tree = ete3.Tree(inputfile, format=1)
+        seen_labels = {}
+        for leaf in tree.iter_leaves():
+            sp = ultimate_seq2sp(leaf.name, ensembl_version)
+            if dots:
+                sp = sp.replace(' ', '.')
+            leaf.name = labelfmt.format(gene=leaf.name, sp=sp)
+
+            if leaf.name in seen_labels:
+                leaf.name += '.%d' % seen_labels[leaf.name]
+                seen_labels[leaf.name] += 1
+            else:
+                seen_labels[leaf.name] = 1
+        tree.write(outfile=outfile, format=1)
+    else:
+        SeqIO.write(iter_specify(inputfile, labelfmt, format, dots, ensembl_version),
+                    outfile, format)
+        #SeqIO.write(iter_specify(SeqIO.parse(inputfile, format),
+        #            labelfmt,  dots, ensembl_version)
+
+#def tree_specify(inputfile, outfile, labelfmt=
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('inputfile')
     parser.add_argument('outfile') 
+    parser.add_argument('-l', '--labelfmt', default="{gene}_{sp}",
+                        help='[%(default)s]')
     parser.add_argument('-f', '--format', default="fasta",
                         help='[%(default)s]')
     parser.add_argument('-e', '--ensembl-version', type=int,
