4e66fda1b3f07a631406706bf9cf4d3fb6ea41be
Guillaume Louvel <7593801+Gullumluvl@users.noreply.github.com>
2020-01-13 17:09:37 +0100

    Include converter code originally in LibsDyogen_py3

--- dendro/converters.py
+++ dendro/converters.py
@@ -43,8 +43,56 @@ def ProtTree_to_ete3(prottree):
             current_nodes.append(current.add_child(name=child, dist=dist))
     return tree
 
-def PhylTree_to_ete3(phyltree):
-    return phyltree.to_ete3()
+
+def PhylTree_to_ete3(phyltree, nosinglechild=False):
+    # TODO: do not import ete3 here, just try to use it and raise error
+    import ete3
+    tree = ete3.Tree(name=phyltree.root, dist=getattr(phyltree, 'rootlength', 0))
+    tree.add_features(treename=getattr(phyltree, 'name', ''))
+    current_nodes = [tree]
+    while current_nodes:
+        current = current_nodes.pop()
+        current.add_features(age=phyltree.ages.get(current.name),
+                             commonNames=phyltree.commonNames.get(current.name),
+                             fileName=phyltree.fileName.get(current.name),
+                             indBranch=phyltree.indBranches.get(current.name),
+                             indName=phyltree.indNames.get(current.name),
+                             )
+                             #officialName=phyltree.officialName.get(current.name),
+        childlist = phyltree.items.get(current.name, [])
+        if not childlist:
+            current.add_features(Esp2X=(current.name in phyltree.lstEsp2X),
+                                 Esp6X=(current.name in phyltree.lstEsp6X),
+                                 EspFull=(current.name in phyltree.lstEspFull))
+            # dicGenes
+            # dicGenomes
+        while nosinglechild and (len(childlist) == 1):
+            current.name = childlist[0][0]
+            current.dist += childlist[0][1]
+            childlist = phyltree.items.get(current.name, [])
+
+        for child, dist in childlist:
+            current_nodes.append(current.add_child(name=child, dist=dist))
+    return tree
+
+
+def PhylTree_draw(phyltree, images=None):
+    """Draw tree in a gui window using Ete3"""
+    import ete3
+    ptree = phyltree.to_ete3()
+    ts = ete3.TreeStyle()
+    ts.scale = 2
+    ts.show_branch_length = True
+    nameface = ete3.faces.AttrFace("name", fsize=12)
+    def mylayout(node):
+        if node.is_leaf():
+            # add species image
+            pass
+        else:
+            ete3.faces.add_face_to_node(nameface, node, column=0,
+                                        position='branch-top')
+    ptree.show(layout=mylayout, tree_style=ts, name='Species tree')
+
 
 def ete3_to_ete3(tree):
     return tree
