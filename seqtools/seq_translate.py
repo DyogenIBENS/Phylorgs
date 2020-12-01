7857aae9bf7ecc70a23fb9e9deb2be2f5e34afbb
Guillaume Louvel <7593801+Gullumluvl@users.noreply.github.com>
2020-12-01 22:05:06 +0100

    Read from the PAML multi-phylip format.

--- seqtools/seq_translate.py
+++ seqtools/seq_translate.py
@@ -1,31 +1,85 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+
+_epilog = ("Note: if input format is \"evolver\" and output format requires "
+"one file per alignment (e.g."
+"regular fasta or phylip), the outfile argument should include a formatter string:\n\n"
+"Example: ./seq_translate.py -f evolver mc.paml output_prot_%02d.phy")
+
+
 import sys
 import argparse
 from Bio import SeqIO
+from Bio.Alphabet import IUPAC
+
+try:
+    from seqtools.seqconv import split_evolver
+except ImportError:
+    print('seqtools.seqconv.split_evolver not available for PAML input format.', file=sys.stderr)
 
 
-def iter_translate(inputfile, format="fasta"):
+def iter_translate(records, format="fasta"):
+    for record in records:
+        tr_record = record.translate(gap='-', id=True, name=True, description=True)
+        assert len(tr_record.seq) > 0
+        yield tr_record
+
+
+def translate(inputfile, outfile, input_format="fasta", output_format='fasta'):
     if inputfile == '-':
         inputfile = sys.stdin
-    for record in SeqIO.parse(inputfile, format):
-        record.seq = record.seq.translate(gap='-')
-        assert len(record.seq) > 0
-        yield record
+    SeqIO.write(iter_translate(SeqIO.parse(inputfile, input_format, alphabet=IUPAC.ambiguous_dna)), outfile, output_format)
 
-def main(inputfile, outfile, format="fasta"):
-    SeqIO.write(iter_translate(inputfile), outfile, format)
+
+def main():
+    parser = argparse.ArgumentParser(description=__doc__, epilog=_epilog)
+    parser.add_argument('inputfile', nargs='?', default=sys.stdin)
+    parser.add_argument('outfile', nargs='?', default=sys.stdout)
+    parser.add_argument('-if', '--input-format', default="fasta", 
+                        help=('Formats for Biopython (fasta, phylip, phylip-'
+                        'sequential, phylip-relaxed, etc.) or "evolver" for '
+                        'the output of PAML evolver. [%(default)s]'))
+    parser.add_argument('-of', '--output-format', default="fasta",
+                        help='[%(default)s]')
+
+    args = parser.parse_args()
+    if args.input_format == 'evolver':
+        # This is just a concatenated Phylip, with extra blank lines.
+        try:
+            if args.outfile == sys.stdout:
+                out = sys.stdout
+                joint_out = True
+            elif args.output_format == 'evolver':
+                out = open(args.outfile, 'w')
+                joint_out = True
+            else:
+                joint_out = False
+
+            if args.inputfile == '-' or args.inputfile == sys.stdin:
+                f = sys.stdin
+            else:
+                f = open(args.inputfile)
+            for aldata in split_evolver(f):
+                if not joint_out:
+                    out = open(args.outfile % n_al, 'w')
+                else:
+                    out.write('\n\n')
+                #print(''.join(line[:20].rstrip() + '\n' for line in al_lines))
+                translate(aldata, out, 'phylip-sequential', args.output_format)
+                if joint_out:
+                    out.write('\n\n')
+                else:
+                    out.close()
+        except BaseException as err:
+            if args.outfile == sys.stdout:
+                f.close()
+                out.close()
+            raise
+
+    else:
+        translate(**vars(args))
 
 
 if __name__=='__main__':
-    parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('inputfile', nargs='?', default=sys.stdin)
-    parser.add_argument('outfile', nargs='?', default=sys.stdout)
-    parser.add_argument('-f', '--format', default="fasta", 
-                        help='[%(default)s]')
-    
-    args = parser.parse_args()
-    main(**vars(args))
-
-
+    main()
--- seqtools/seqconv.py
+++ seqtools/seqconv.py
@@ -2,9 +2,59 @@
 # -*- coding: utf-8 -*-
 
 
-from sys import stdin, stdout
+from sys import stdin, stdout, stderr
 import argparse
-from Bio import AlignIO
+import re
+from io import StringIO
+from Bio import SeqIO, AlignIO
+
+
+EXT2FMT = {'fa': 'fasta', 'fasta': 'fasta', 'mfa': 'fasta',
+           'phylip': 'phylip-relaxed',
+           'nx': 'nexus',
+           'paml': 'evolver'}
+
+
+PHYLIP_HEAD_REG = re.compile(r'^\s*\d+\s\d+$')
+
+
+def guess_format(filename, default='fasta'):
+    if filename in (stdin, stdout, '-'):
+        return default
+    ext = filename.rsplit('.', 1)[1].split(':')[0]
+    # specifications for the number of alignments are specified after colon
+    return EXT2FMT[ext]
+
+
+def split_evolver(f, start=0, end=None):
+    al_lines = []
+    n_al = 0
+    for line in f:
+        if line.rstrip():
+            if PHYLIP_HEAD_REG.match(line.rstrip()):
+                #print('Phylip start (len(al_lines) = %d)' % len(al_lines))
+                if al_lines:
+                    assert len(al_lines) == nseq+1
+
+                    if n_al>=start:
+                        #yield SeqIO.parse(StringIO(''.join(al_lines)), 'phylip-sequential')
+                        yield StringIO(''.join(al_lines))
+
+                    al_lines = []
+                    n_al += 1
+                    if end is not None and n_al > end:
+                        print('Parsed %d alignments.' % (n_al-start), file=stderr)
+                        return
+                nseq, length = [int(x) for x in line.split()]
+                al_lines = [line]
+            else:
+                #print('+ %r...' % line[:20])
+                al_lines.append(line)
+    assert len(al_lines) == nseq+1
+    #print(''.join(line[:20].rstrip() + '\n' for line in al_lines))
+
+    yield StringIO(''.join(al_lines))
+    print('Parsed %d alignments.' % (n_al+1), file=stderr)
 
 
 def write_phylip_sequential_relaxed(al, outfile):
@@ -12,7 +62,7 @@ def write_phylip_sequential_relaxed(al, outfile):
     - phylip (strict, interleaved),
     - phylip-sequential (strict, sequential),
     - phylip-relaxed (relaxed, interleaved),
-      
+
     but not a relaxed sequential (as needed for PhyloBayes).
     
     Write sequences on a single line.
@@ -29,15 +79,49 @@ def write_phylip_sequential_relaxed(al, outfile):
             outfile.close()
 
 
-def convert_to_phylip_sequential_relaxed(infile, outfile, fro="fasta"):
-    write_phylip_sequential_relaxed(AlignIO.read(infile, fro), outfile)
-
-
-def main(infile, outfile, fro="fasta", to="phylip-relaxed"):
+def convert_one_dataset(infile, out, fro='fasta', to='phylip-relaxed'):
     if to == 'phylip-sequential-relaxed':
-        convert_to_phylip_sequential_relaxed(infile, outfile, fro)
+        write_phylip_sequential_relaxed(AlignIO.read(infile, fro), outfile)
     else:
-        AlignIO.convert(infile, fro, outfile, to)
+        AlignIO.convert(infile, fro, out, to)
+
+
+def main(infile, outfile, fro=None, to=None):
+    if fro is None:
+        fro = guess_format(infile)
+    if to is None:
+        to = guess_format(outfile, 'phylip-relaxed')
+
+    if infile == '-':
+        infile = stdin
+    if outfile not in (stdout, '-'):
+        out = open(outfile, 'w')
+    else:
+        out = stdout
+
+    if fro == 'evolver':
+        ext = infile.rsplit('.', 1)[1]
+        if ':' in ext:
+            infile, selection = infile.rsplit(':', 1)
+            start, end = re.match(r'(\d+)(?:-(\d*))?$', selection).groups()
+            start = int(start)
+            if end is None:
+                end = start
+            elif end == '':
+                end = None  # read the entire file
+            else:
+                end = int(end)
+        else:
+            start, end = 0, None
+        with open(infile) as f:
+            for aldata in split_evolver(f, start, end):
+                convert_one_dataset(aldata, out, 'phylip-sequential', to)
+                out.write('\n\n')
+    else:
+        convert_one_dataset(infile, out, fro, to)
+
+    if outfile not in (stdout, '-'):
+        out.close()
 
 
 if __name__ == '__main__':
@@ -46,9 +130,10 @@ if __name__ == '__main__':
                         default=stdin)
     parser.add_argument('outfile', nargs='?', #type=argparse.FileType('w'),
                         default=stdout)
-    # TODO: inplace edition (hum, bad idea)
-    parser.add_argument('-f', '--from', dest='fro', default='fasta')
-    parser.add_argument('-t', '--to', default='phylip-relaxed')
+    parser.add_argument('-f', '--from', dest='fro',
+                        help='[guess from extension, or fasta if stdin]')
+    parser.add_argument('-t', '--to',
+                        help='[guess from extension, or phylip-relaxed if stdout]')
     
     args = parser.parse_args()
     main(**vars(args))
