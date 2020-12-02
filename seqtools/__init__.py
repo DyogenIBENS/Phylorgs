9e4b479ea4f85c4f01f334d8a52a13172e4e06a7
Guillaume Louvel <7593801+Gullumluvl@users.noreply.github.com>
2020-12-02 16:39:08 +0100

    less heavy loading of seqtools (because plot_al_conservation has a lot of heavy plotting libraries like seaborn mostly unnecessary for quick command line tasks)

--- seqtools/__init__.py
+++ seqtools/__init__.py
@@ -4,7 +4,4 @@
 from .ungap import ungap
 from .seqname_grep import seqrecords_grep, algrep
 from .compo_freq import make_al_compo
-from .plot_al_conservation import reorder_al, get_position_stats
-
-
 
