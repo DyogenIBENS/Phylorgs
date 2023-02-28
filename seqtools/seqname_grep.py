59c7b908d060f5aae4f8e0760dda57ec2cce912e
Guillaume Louvel <7593801+Gullumluvl@users.noreply.github.com>
2023-02-28 21:13:09 +0100

    More grep-like options in seqname_grep: fixed pattern, word pattern, line pattern (=label)

--- seqtools/seqname_grep.py
+++ seqtools/seqname_grep.py
@@ -27,16 +27,30 @@ def algrep(alignment, pattern, negate=False):
     return Align.MultipleSeqAlignment(out_records)
 
 
-def main(pattern, filename, negate=False, fromfile=False, format='fasta', outfile=stdout):
-    #al = AlignIO.read(alignment_file, format=format)
+def main(pattern, filename, negate=False, fromfile=False, fixed=False, word=False, label=False, seq_format='fasta', outfile=stdout):
+    #al = AlignIO.read(alignment_file, format=seq_format)
+
+    if label:
+        begin, end = r'^', r'$'
+    elif word:
+        begin, end = r'\b', r'\b'
+    else:
+        begin, end = '', ''
+    if fixed:
+        def wrap_pattern_elem(pat):
+            return begin + re.escape(pat) + end
+    else:
+        def wrap_pattern_elem(pat):
+            return begin + pat + end
+
     if fromfile:
         if pattern == '-':
-            pattern = '|'.join(line.rstrip() for line in stdin)
+            pattern = '|'.join(wrap_pattern_elem(line.rstrip()) for line in stdin)
         else:
             with open(pattern) as f:
-                pattern = '|'.join(line.rstrip() for line in f)
-    sequences = SeqIO.parse(filename, format=format)
-    SeqIO.write(seqrecords_grep(sequences, pattern, negate), outfile, format)
+                pattern = '|'.join(wrap_pattern_elem(line.rstrip()) for line in f)
+    sequences = SeqIO.parse(filename, format=seq_format)
+    SeqIO.write(seqrecords_grep(sequences, pattern, negate), outfile, seq_format)
 
 
 if __name__ == '__main__':
@@ -48,8 +62,11 @@ if __name__ == '__main__':
                         default=stdout)
     parser.add_argument('-f', '--fromfile', action='store_true',
                         help='Read pattern from file (one alternative per line).')
-    parser.add_argument('-F', '--format', default='fasta')
+    parser.add_argument('-F', '--fixed', action='store_true', help='Disable regex')
+    parser.add_argument('-w', '--word', action='store_true', help='Only match whole words')
+    parser.add_argument('-x', '--label', action='store_true', help='Only match whole label')
     parser.add_argument('-v', '--negate', action='store_true')
-    
+    parser.add_argument('-s', '--seq-format', default='fasta')
+
     args = parser.parse_args()
     main(**vars(args))
